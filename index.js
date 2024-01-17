1716bc422e08966ea501a8207865b662d06be68f
Siv Earley <searley96@gmail.com>
2024-01-17 14:58:15 -0600

    worked on cleanInputString function

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
+let isError = false
+
+//number in str array will be a string, use this function to parse string into idv characters
+//put idv characters into new array
+//for loop iterates through each character in strArray, check if character in strArray at index i is not a +, -, or a space
+//If its not, push to the cleanStrArray
+function cleanInputString(str) {
+    const strArray = str.split('');
+    const cleanStrArray = []
+    for (let i = 0; i < strArray.length; i++) {
+        if (!["+", "-", " "].includes(strArray[i])) {
+            cleanStrArray.push(strArray[i]);
+          }
+    }
+}
