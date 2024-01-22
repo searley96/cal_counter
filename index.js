83c5fef6a058c88fc1f0b335152e3c852a2766b7
Siv Earley <searley96@gmail.com>
2024-01-22 12:39:10 -0600

    finished getCaloriesFromInputs function

--- index.js
+++ index.js
@@ -40,8 +40,27 @@ function addEntry() {
       id="${entryDropdown.value}-${entryNumber}-calories"
       placeholder="Calories"
     />`;
-    targetInputContainer.insertAdjacentHTML();
+    targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 
   }
 
   addEntryButton.addEventListener("click", addEntry);
+
+  function getCaloriesFromInputs(list) {
+    let calories = 0;
+  
+    for (let i = 0; i < list.length; i++) {
+      const currVal = cleanInputString(list[i].value);
+      const invalidInputMatch = isInvalidInput(currVal);
+  
+      if (invalidInputMatch) {
+        alert(`Invalid Input: ${invalidInputMatch[0]}`);
+        isError = true;
+        return null;
+      }
+      calories += Number(currVal);
+    }
+    return calories 
+  }
+
+  
